c28fab4d9a7c350f3deddbc00ac573e7c198afd9
Jun Wang <jun.wang@cvshealth.com>
2020-03-05 11:52:28 -0600

    compare JEST and Mocha

--- HelloJest/tests/ConvertSuperScript.test.ts
+++ HelloJest/tests/ConvertSuperScript.test.ts
@@ -1,10 +1,10 @@
 import converter from "../src/CovertSuperScript";
+let input = 'convert XIP';
 
 describe("Covert to superscript", () => {
   it("superscript", () => {
-    let input ='convert XIP'; 
     let result = converter.CovertSuperscript(input);
     console.log(converter.CovertSuperscript(input));
-    expect(result).toBe('<sup>'+input+'</sup>');
+    expect(result).toBe('<sup>' + input + '</sup>');
   });
 });
