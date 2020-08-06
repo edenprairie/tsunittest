370e18140eb28c2489d6a9958688e2d4a1d4d6ca
Jun Wang <junwang01@gmail.com>
2020-08-05 22:06:05 -0500

    update test code

--- HelloJasmine/tests/ConvertSuperScript.test.ts
+++ HelloJasmine/tests/ConvertSuperScript.test.ts
@@ -6,7 +6,7 @@ describe("Covert to superscript", () => {
     let input ='convert XIP'; 
     let result = converter.CovertSuperscript(input);
     console.log(converter.CovertSuperscript(input));
-    expect(result).toBe('<sup>'+input+'</suip>');
+    expect(result).toBe('<sup>'+input+'</sup>');
   });
   
 });
